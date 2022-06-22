a72fa4e2cc2419426b5f91598877de169a6beb58
darrenl <darrenl@ZAKZRHB-CLLB24.ballito.local>
2022-06-22 11:35:30 +0200

    Text rendere for Text Areas

---
+++ src/main/java/backend/TextRenderer.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package backend;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author darrenl
+ */
+
+public class TextRenderer implements Runnable {
+    private JTextArea jta;
+    private int delay;
+    private String filePath;
+
+    public TextRenderer(JTextArea in, int delay, String inFilePath) {
+        this.jta = in;
+        this.delay = delay;
+        this.filePath = inFilePath;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            File bootText = new File(filePath);
+
+            Scanner bootSC = new Scanner(bootText);
+
+            String out = "";
+
+            while (bootSC.hasNextLine()) {
+                out += bootSC.nextLine() + "\n";
+            }
+
+            for (int i = 0; i < out.length(); i++) {
+                jta.setText(out.substring(0, i));//message.charAt(i);
+                Thread.sleep(delay);
+            }
+
+        } catch (InterruptedException e) {
+
+        } catch (FileNotFoundException e) {
+
+        }
+    }
+
+}
