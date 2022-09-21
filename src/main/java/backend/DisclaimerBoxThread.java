5eb9245db3c9d760275b0ec8e2aae76010c80872
darrenl <darrenl@ZAKZRHB-CLLB24.ballito.local>
2022-09-21 13:01:33 +0200

    Edited code for this thread and added image for FBI insane montage scene at the end

--- src/main/java/backend/DisclaimerBoxThread.java
+++ src/main/java/backend/DisclaimerBoxThread.java
@@ -26,13 +26,40 @@ public class DisclaimerBoxThread implements Runnable{
     private JButton agree5;
     
     public DisclaimerBoxThread(JCheckBox NDA, JCheckBox LossOrHarm, JCheckBox Death, JCheckBox PainAndMurder, JCheckBox LossOfLimbs, JCheckBox Censorship, JCheckBox ToHaveAGoodTime, JButton agree1, JButton agree2, JButton agree3, JButton agree4, JButton agree5){
-        
+        this.NDA = NDA;
+        this.LossOrHarm = LossOrHarm;
+        this.Death = Death;
+        this.PainAndMuder = PainAndMurder;
+        this.LossOfLimbs = LossOfLimbs;
+        this.Censorship = Censorship;
+        this.ToHaveAGoodTime = ToHaveAGoodTime;
+        this.agree1 = agree1;
+        this.agree2 = agree2;
+        this.agree3 = agree3;
+        this.agree4 = agree4;
+        this.agree5 = agree5;
     }
+    
     
     
 
     @Override
     public void run() {
+        NDA.setVisible(false);
+        LossOrHarm.setVisible(false);
+        Death.setVisible(false);
+        PainAndMuder.setVisible(false);
+        LossOfLimbs.setVisible(false);
+        Censorship.setVisible(false);
+        ToHaveAGoodTime.setVisible(false);
+        agree1.setVisible(false);
+        agree2.setVisible(false);
+        agree3.setVisible(false);
+        agree4.setVisible(false);
+        agree5.setVisible(false);
+        
+        
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
